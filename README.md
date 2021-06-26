5cf18b178d9434948b1cc9907b3c663e33d4920b
Yalim Demirkesen <yalimdemirkesen@gmail.com>
2021-06-26 10:39:59 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# pycohort
+
+analyzing the retention rates
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycohort',
+      version='2.7',
+      description='pycohort package',
+      url='https://github.com/demirkeseny',
+      author='Yalim Demirkesen',
+      author_email='yalimdemirkesen@gmail.com',
+      license='MIT',
+      packages=['pycohort'],
+      zip_safe=False)
